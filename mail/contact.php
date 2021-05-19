ad38e01e25acd3fde0c7315af00ce0fde51c4306
batyrkanov <artez994@gmail.com>
2021-05-19 16:35:25 +0600

    added mail sending

---
+++ mail/contact.php
@@ -0,0 +1,19 @@
+<?php
+if(empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "info@taco.global"; // Change this email to your //
+$subject = "$m_subject";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
